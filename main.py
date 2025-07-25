157a4829ea6160e9c860bbac9c4d6ef3cd81dee8
Vikesh S <vikesh024@gmail.com>
2025-07-25 11:13:22 +0530

    feat: Add books endpoint

--- main.py
+++ main.py
@@ -1,11 +1,17 @@
 from fastapi import FastAPI
 import uvicorn
+import uuid
 
 app = FastAPI()
 
-@app.get("/")
-def read_root():
-    return {"Hello": "World"}
+@app.get("/books/{book_id}")
+def read_root(book_id: int):
+    return {
+        "job_id": uuid.uuid4(),
+        "book_id": book_id,
+        "title": "The Great Gatsby",
+        "author": "F. Scott Fitzgerald"
+    }
 
 
 if __name__ == "__main__":
