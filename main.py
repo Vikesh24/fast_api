87e1a4c63c9b2ba1dace71c21fd9dc37ad97e457
Vikesh S <vikesh024@gmail.com>
2025-07-25 11:46:18 +0530

    refactor: Add books and authors router

--- main.py
+++ main.py
@@ -1,18 +1,16 @@
 from fastapi import FastAPI
 import uvicorn
-import uuid
+
+from books_router import router as books_router
+from authors_router import router as authors_router
 
 app = FastAPI()
+app.include_router(books_router)
+app.include_router(authors_router)
 
-@app.get("/books/{book_id}")
-def read_root(book_id: int):
-    return {
-        "job_id": uuid.uuid4(),
-        "book_id": book_id,
-        "title": "The Great Gatsby",
-        "author": "F. Scott Fitzgerald"
-    }
-
+@app.get("/")
+def welcome():
+    return "Welcome to the world of FastAPI mamey"
 
 if __name__ == "__main__":
     uvicorn.run("main:app", reload=True, port=3000)
